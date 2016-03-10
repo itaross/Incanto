dad442cd264cb2389f8f4664e7880447e9fc9b06
= <sa067265@SMAWS23767170.saipemnet.saipem.intranet>
2016-03-10 12:01:12 +0100

    Added files and test app

--- IncantoTestApp/MainWindow.xaml.cs
+++ IncantoTestApp/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Incanto;
+using Incanto.Themes;
+using Incanto.Controls;
+using Incanto.Helpers;
 
 namespace IncantoTestApp
 {
@@ -20,9 +24,49 @@ namespace IncantoTestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public CurrentTheme theme;
         public MainWindow()
         {
             InitializeComponent();
+
+            //foreach (var themed_control in ControlsFinder.GetLogicalChildren<IncantoControl>(this))
+            //{
+            //}
+            //theme = CurrentTheme.LightTheme;
+            ChangeToLightTheme();
+        }
+
+        private void ChangeToLightTheme()
+        {
+            ThemeEngine.SetCurrentThemeDictionary(this, new Uri("pack://application:,,,/Incanto;component/Themes/LightTheme.xaml"));
+            theme = CurrentTheme.LightTheme;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (theme == CurrentTheme.DarkTheme)
+            {
+                foreach (var themed_control in ControlsFinder.GetLogicalChildren<IncantoControl>(this))
+                {
+                    ThemeEngine.SetCurrentThemeDictionary(this, new Uri("pack://application:,,,/Incanto;component/Themes/LightTheme.xaml"));
+                }
+                theme = CurrentTheme.LightTheme;
+            }
+            else
+            {
+                foreach (var themed_control in ControlsFinder.GetLogicalChildren<IncantoControl>(this))
+                {
+                    ThemeEngine.SetCurrentThemeDictionary(this, new Uri("pack://application:,,,/Incanto;component/Themes/DarkTheme.xaml"));
+                }
+                theme = CurrentTheme.DarkTheme;
+            }
+        }
+
+        public enum CurrentTheme
+        {
+            DarkTheme,
+            LightTheme
+
         }
     }
 }

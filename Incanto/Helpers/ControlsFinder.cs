3bdffe083e8cbb82f080f5799d20b3e121531c20
unknown <sa067265@SMAWS23767170.saipemnet.saipem.intranet>
2016-03-10 11:58:55 +0100

    Add light theme. Themes to be further expanded with new color definitions

---
+++ Incanto/Helpers/ControlsFinder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Incanto.Helpers
+{
+    public class ControlsFinder
+    {
+        private static StringBuilder childrenTreeInfo;
+        public static StringBuilder ChildrenTreeInfo
+        {
+            get
+            {
+                return childrenTreeInfo != null ? childrenTreeInfo : new StringBuilder();
+            }
+
+            private set { }
+        }
+
+
+        public static List<T> GetLogicalChildren<T>(object parent) where T : DependencyObject
+        {
+            List<T> logicalCollection = new List<T>();
+            childrenTreeInfo = new StringBuilder();
+            GetLogicalChildCollection(parent as DependencyObject, logicalCollection);
+            return logicalCollection;
+        }
+
+        private static void GetLogicalChildCollection<T>(DependencyObject parent, List<T> logicalCollection) where T : DependencyObject
+        {
+            IEnumerable children = LogicalTreeHelper.GetChildren(parent);
+            foreach (object child in children)
+            {
+                if (child is DependencyObject)
+                {
+                    DependencyObject depChild = child as DependencyObject;
+                    if (child is T && child != null)
+                    {
+                        logicalCollection.Add(child as T);
+                        childrenTreeInfo.Append(child.GetType());
+                    }
+
+                    GetLogicalChildCollection(depChild, logicalCollection);
+                }
+            }
+        }
+
+    }
+}
---
+++ Incanto/IncantoDefs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Incanto
+{
+    public class IncantoDefs
+    {
+        public static string ThemesLocation = @"pack://application,,,/Incanto;Themes/";
+        public static string DarkThemeLocation  = ThemesLocation + "DarkTheme.xaml";
+        public static string LightThemeLocation = ThemesLocation + "LightTheme.xaml";
+    }
+}
